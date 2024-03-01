86d57c570a308e2713de5c4be4ebd105064d5e7a
Jeff Lewis <jeff.lewis@acryl.io>
2024-03-01 13:50:16 -0600

    Refactor neo4j_dataset.py: Reorder imports, add graph initialization, and emit metadata change proposals

--- examples/neo4j_dataset.py
+++ examples/neo4j_dataset.py
@@ -1,7 +1,9 @@
 # Inlined from /metadata-ingestion/examples/library/dataset_schema.py
 # Imports for urn construction utility methods
-from datahub.emitter.mce_builder import make_data_platform_urn, make_dataset_urn
+from datahub.emitter.mce_builder import make_dataset_urn, make_data_platform_urn
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
+from datahub.ingestion.graph.client import DataHubGraph, get_default_graph
+
 
 # Imports for metadata model classes
 from datahub.metadata.schema_classes import (
@@ -12,12 +14,18 @@ from datahub.metadata.schema_classes import (
     SchemaFieldDataTypeClass,
     SchemaMetadataClass,
     StringTypeClass,
+    SubTypesClass,
+    UpstreamClass,
+    DatasetLineageTypeClass,
+    UpstreamLineageClass
 )
 
 import time
 
 from pydantic import BaseModel
 from typing import List, Dict, Union, Any, Optional, Tuple, Iterable
+
+graph: DataHubGraph = get_default_graph()
 
 class neo4jDataset(BaseModel):
     name: str
@@ -59,21 +67,47 @@ fields = [ SchemaFieldClass(
             description=field["description"],
         ) for field in source_dataset.fields]
 
-
-event: MetadataChangeProposalWrapper = MetadataChangeProposalWrapper(
-    entityUrn=make_dataset_urn(platform="neo4j", name="default.customer", env="PROD"),
-    aspect=SchemaMetadataClass(
-        schemaName="customer",  # not used
-        platform=make_data_platform_urn("neo4j"),  # important <- platform must be an urn
-        version=0,  # when the source system has a notion of versioning of schemas, insert this in, otherwise leave as 0
-        hash="",  # when the source system has a notion of unique schemas identified via hash, include a hash, else leave it as empty string
-        platformSchema=OtherSchemaClass(rawSchema="__insert raw schema here__"),
-        lastModified=current_timestamp,
-        fields=fields,
-    ),
+subtypes = SubTypesClass(
+    typeNames = ["Node"],
 )
 
-print(event)
+# target_dataset = make_dataset_urn(
+#     source_dataset.platform, 
+#     f"{source_dataset.name}",
+# )
+
+target_dataset = "urn:li:dataset:(urn:li:dataPlatform:snowflake,banking.public.account,PROD)"
+
+upstream_dataset = ( 
+    UpstreamClass(
+        dataset=target_dataset,
+        type=DatasetLineageTypeClass.TRANSFORMED
+    )
+)
+
+lineage = UpstreamLineageClass(
+    [upstream_dataset]
+)
+
+mcps = MetadataChangeProposalWrapper.construct_many(
+    entityUrn=make_dataset_urn(platform="neo4j", name="default.customer", env="PROD"),
+    aspects=[
+        SchemaMetadataClass(
+            schemaName="customer",  # not used
+            platform=make_data_platform_urn("neo4j"),  # important <- platform must be an urn
+            version=0,  # when the source system has a notion of versioning of schemas, insert this in, otherwise leave as 0
+            hash="",  # when the source system has a notion of unique schemas identified via hash, include a hash, else leave it as empty string
+            platformSchema=OtherSchemaClass(rawSchema="__insert raw schema here__"),
+            lastModified=current_timestamp,
+            fields=fields,
+        ),
+    subtypes,
+    lineage
+    ],
+)   
+
+for mcp in mcps:
+    graph.emit(mcp)
 
 # Create rest emitter
 # rest_emitter = DatahubRestEmitter(gms_server="http://localhost:8080")
